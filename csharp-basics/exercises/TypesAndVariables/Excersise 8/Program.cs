a2fc9374031d4e9e516c40a1b9fdc2ef7a9847dc
Toms <toms.stasulans@gmail.com>
2021-11-25 19:00:25 +0200

    Excersise 8 done.

---
+++ csharp-basics/exercises/TypesAndVariables/Excersise 8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Excersise_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number of minutes");
+            int minutes = int.Parse(Console.ReadLine());
+
+            int years = minutes / 525600;
+            int days = minutes / 1440;
+
+            Console.WriteLine("{0} minutes are {1} years or {2} days", minutes, years, days);
+            Console.ReadKey();
+        }
+    }
+}
