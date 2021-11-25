09ad954b5555e540790986ae9dc91a3867ff047b
Toms <toms.stasulans@gmail.com>
2021-11-25 17:37:21 +0200

    Excersise 7 done.

---
+++ csharp-basics/exercises/TypesAndVariables/Excersise 7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersise_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentence");
+
+            string sentence = Console.ReadLine();
+            int upperCaseLetterCount = sentence.Count(letter => char.IsUpper(letter));
+
+            Console.WriteLine("You have {0} upper case letters in your sentence", upperCaseLetterCount);
+            Console.ReadLine();
+        }
+    }
+}
