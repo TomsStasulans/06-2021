5b8cb1376e71118c65df80199084e897c0a4bc7e
Toms <toms.stasulans@gmail.com>
2021-11-26 13:20:25 +0200

    Exercise 1 done

---
+++ csharp-basics/exercises/Arithmetic/Exercise 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter first integer");
+            int firstInt = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter second integer");
+            int secondInt = int.Parse(Console.ReadLine());
+
+            if (firstInt == 15 || secondInt == 15)
+            {
+                Console.WriteLine("true");
+            } else if (firstInt - secondInt == 15 || firstInt + secondInt == 15)
+            {
+                Console.WriteLine("true");
+            } else
+            {
+                Console.WriteLine("false");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
