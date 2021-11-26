b1c38a14cfe2762518a383ef092f5e710b7b29cd
Toms <toms.stasulans@gmail.com>
2021-11-26 15:11:16 +0200

    Exercise 6 done.

---
+++ csharp-basics/exercises/Arithmetic/Exercise 6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 110; i++)
+            {
+                string str = "";
+                if (i % 3 == 0)
+                {
+                    str += "Coza";
+                } 
+                
+                if (i % 5 == 0)
+                {
+                    str += "Loza";
+                }
+                
+                if (i % 7 == 0)
+                {
+                    str += "Woza";
+                }
+                 if(i % 3 != 0 && i % 5 != 0 && i % 7 != 0)
+                {
+                    str += i;
+                }
+
+                Console.WriteLine(str);
+            }
+            Console.ReadKey();
+        }
+    }
+}
