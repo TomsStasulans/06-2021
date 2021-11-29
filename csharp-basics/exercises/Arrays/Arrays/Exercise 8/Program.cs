f1d38d81c6017e9a00147a61033923f48d1653d1
Toms <toms.stasulans@gmail.com>
2021-11-29 20:50:06 +0200

    partly done

--- csharp-basics/exercises/Arrays/Arrays/Exercise 8/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise 8/Program.cs
@@ -10,6 +10,42 @@ namespace Exercise_8
     {
         static void Main(string[] args)
         {
+            string[] words = { "ola", "jaka", "maks", "lelle" };
+            var randomNumber = new Random();
+            var word = words[randomNumber.Next(0, words.Length)];
+
+            var guessable = new string('_', word.Length);
+            var misses = string.Empty;
+
+            while (guessable.IndexOf('_') != -1)
+            {
+                Console.WriteLine($"word: {guessable}");
+                Console.WriteLine($"misess:");
+                var input = Console.ReadKey();
+                var guess = input.KeyChar;
+                Console.WriteLine(guess);
+                Console.WriteLine($"Guess: {guess}");
+                
+
+                if (word.IndexOf(guess) > -1)
+                {
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (word[i] == guess)
+                        {
+                            var guessableWord = new StringBuilder(guessable);
+                            guessableWord[i] = word[i];
+                            guessable = guessableWord.ToString();
+                        }
+                    }
+                }
+                else
+                {
+                    misses += guess;
+                }
+
+                Console.ReadKey();
+            }
         }
     }
 }
