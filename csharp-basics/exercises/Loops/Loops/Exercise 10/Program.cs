66c6005a96178bbd4ce9dcc2653cc04c0b4c2374
Toms <toms.stasulans@gmail.com>
2021-12-01 23:35:58 +0200

    Exercise 10 done.

---
+++ csharp-basics/exercises/Loops/Loops/Exercise 10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter min number");
+            int min = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter max number");
+            int max = int.Parse(Console.ReadLine());
+
+            for (int i = min; i <= max; i++)
+            {
+                string result = "";
+
+                for (int j = i; j <= max; j++)
+                {
+                    result += j;
+                }
+
+                for (int k = min; k < min + i - 1; k++)
+                {
+                    result += k;
+                }
+
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
