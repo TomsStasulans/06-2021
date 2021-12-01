bd18e1ddcea6e93253e9b2142c30571fd8183ac2
Toms <toms.stasulans@gmail.com>
2021-12-01 09:40:10 +0200

    Exercise_8 done.

--- csharp-basics/exercises/Arrays/Arrays/Exercise 8/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise 8/Program.cs
@@ -10,48 +10,67 @@ namespace Exercise_8
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello! Lets play a Hangman.");
             bool WantToPlay = true;
-            var word = ChooseRandomWord();
-            var guessable = new string('_', word.Length);
-            var misses = string.Empty;
-            var tries = 0;
 
-            while (guessable.IndexOf('_') != -1)
+            while (WantToPlay)
             {
-                Console.WriteLine("You guesed: {0} {1}", tries, tries == 1 ? "time" : "times", tries);
-                Console.WriteLine($"word: {guessable}");
-                Console.WriteLine($"misess: {misses}");
-                var input = Console.ReadKey();
-                var guess = input.KeyChar;
-                Console.WriteLine($"\nGuess: {guess}");
-                
+                var word = ChooseRandomWord();
+                var guessable = new string('_', word.Length);
+                var misses = string.Empty;
+                var tries = 0;
 
-                if (IsLetterInWord(word, guess))
+                while (guessable.IndexOf('_') != -1)
                 {
-                    for (int i = 0; i < word.Length; i++)
+                    Console.WriteLine("You guesed: {0} {1}", tries, tries == 1 ? "time" : "times");
+                    Console.WriteLine($"word: {guessable}");
+                    Console.WriteLine($"misess: {misses}");
+                    var inputs = Console.ReadKey();
+                    var guess = inputs.KeyChar;
+                    Console.WriteLine($"\nGuess: {guess}");
+
+                    if (IsLetterInWord(word, guess))
                     {
-                        if (word[i] == guess)
+                        for (int i = 0; i < word.Length; i++)
                         {
-                            var guessableWord = new StringBuilder(guessable);
-                            guessableWord[i] = word[i];
-                            guessable = guessableWord.ToString();
+                            if (word[i] == guess)
+                            {
+                                var guessableWord = new StringBuilder(guessable);
+                                guessableWord[i] = word[i];
+                                guessable = guessableWord.ToString();
+                            }
                         }
                     }
+                    else
+                    {
+                        misses += guess;
+                    }
+
+                    tries++;
+
+                    if (misses.Length == 7)
+                    {
+                        Console.WriteLine("You made 7 mistakes. It's too many.");
+                        break;
+                    }
+                    else if (guessable.IndexOf('_') == -1)
+                    {
+                        Console.WriteLine("Nice! You guesed it! The word was {0}.", word);
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Play 'again' or 'quit'?");
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "again")
+                {
+                    continue;
                 }
                 else
                 {
-                    misses += guess;
+                    break;
                 }
-
-                tries++;
-
-                if (misses.Length == 7)
-                {
-                    Console.WriteLine("You made 7 mistakes.It's too many. Try again?");
-                }
-
-
-                
             }
         }
 
