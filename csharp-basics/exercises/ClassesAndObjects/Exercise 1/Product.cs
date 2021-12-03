b5d6df3ca557c2c2273466e3b898a40b1815afc2
Toms <toms.stasulans@gmail.com>
2021-12-03 12:26:00 +0200

    Exercise 1 partly done.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 1/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    public class Product
+    {
+        public Product(double priceAtStart, int amountAtStart, string name)
+        {
+            _priceAtStart = priceAtStart;
+            _amountAtStart = amountAtStart;
+            _name = name;
+        }
+
+        private double _priceAtStart { get; set; }
+        private int _amountAtStart { get; set; }
+        private string _name;
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{_name}, {_priceAtStart}, {_amountAtStart} ");
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product mouse = new Product(70.00, 14, "Logitech mouse");
+            mouse.PrintProduct();
+            Product phone = new Product(999.99, 3, "iPhone 5s");
+            phone.PrintProduct();
+            Product epson = new Product(440.46, 1, "Epson EB-U05");
+            epson.PrintProduct();
+            Console.ReadKey();
+        }
+    }
+}
