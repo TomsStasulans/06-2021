8cc3f7e1e8e8b9f44fbdd526d28dd49bb234fe95
Toms <toms.stasulans@gmail.com>
2021-12-06 09:55:02 +0200

    Exercise 3 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/Ecercise 3/Odometer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecercise_3
+{
+    class Odometer
+    {
+        public double _mileage { get; set; }
+
+        public Odometer(double mileage)
+        {
+            _mileage = mileage;
+        }
+
+        public void Mileage()
+        {
+            Console.WriteLine(_mileage); 
+        }
+
+        public double IncrementMileage()
+        {
+            if (_mileage < 999999)
+            {
+                return _mileage++;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Ecercise 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecercise_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Odometer myOdo = new Odometer(400);
+            FuelGauge myTank = new FuelGauge(3);
+
+            myTank.IncrementFuel();
+
+
+            while(myTank._liters != 0)
+            {
+                myOdo.IncrementMileage();
+                double currentMilage = myOdo._mileage;
+                if(currentMilage % 10 == 0)
+                {
+                    myTank.DecrementFuel();
+                }
+                myTank.ReportLiters();
+                myOdo.Mileage();
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
