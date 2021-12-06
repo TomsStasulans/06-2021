7e8aa09792e3a6192b29bc912528980ffa9646f2
Toms <toms.stasulans@gmail.com>
2021-12-06 16:33:08 +0200

    Exercise 4 fixed.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 4/Movie.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise_4
+{
+    internal class Movie
+    {
+        private string _title { get; set; }
+        private string _studio { get; set; }
+        public string _rating { get; private set; }
+
+        public Movie(string title, string studio, string rating)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            _title = title;
+            _studio = studio;
+            _rating = "PG";
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Exercise_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var casinoRoyal = new Movie("Casino Royale", "Eon Productions", "PG13");
+            var glass = new Movie("Glass", "Buena Vista International", "PG13");
+            var spiderMan = new Movie("Spider-Man: Into the Spider-Verse", "Columbia Pictures", "PG");
+        }
+
+        public static Movie[] GetPG(Movie[] movies)
+        {
+            var filtered = new List<Movie>();
+            foreach (Movie movie in movies)
+            {
+                if (movie._rating == "PG")
+                {
+                    filtered.Add(movie);
+                }
+            }
+
+            return filtered.ToArray();
+        }
+    }
+}
