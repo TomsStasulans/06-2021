3b6221698e962bc6c04405e925be9ae30cc7cbb1
Toms <toms.stasulans@gmail.com>
2021-12-06 17:11:16 +0200

    Exercise 5 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 5/Date.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercise_5
+{
+    internal class Date
+    {
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = month;
+            _day = day;
+        }
+
+        private int _year { get; set; }
+        private int _month { get; set; }
+        private int _day { get; set; }
+
+        public string DisplayDate()
+        {
+            return $"{_month}/{_day}/{_year}";
+        }
+    }
+}
--- csharp-basics/exercises/ClassesAndObjects/Exercise 5/Program.cs
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 5/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Exercise_5
+{
+    internal class DateTest
+    {
+        static void Main(string[] args)
+        {
+            Date today = new Date(2021, 12, 6);
+            Console.WriteLine(today.DisplayDate());
+            Console.ReadKey();
+        }
+    }
+}
