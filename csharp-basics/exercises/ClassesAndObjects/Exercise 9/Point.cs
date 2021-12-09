c0483a1e6d9bc1a16ccb1bfd469475e3057d6581
Toms <toms.stasulans@gmail.com>
2021-12-09 15:41:28 +0200

    Exercise 9 done.

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 9/Point.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise_9
+{
+    internal class Point
+    {
+        public int _x;
+        public int _y;
+
+        public Point(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public Point SwapPoints(Point p1, Point p2)
+        {
+            int tempX = p1._x;
+            int tempY = p1._y;
+            p1._x = p2._x;
+            p1._y = p2._y;
+            p2._x = tempX;
+            p2._y = tempY;
+            return this;
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise 9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(5, 2);
+            Point p2 = new Point(-3, 6);
+            p1.SwapPoints(p1, p2);
+            Console.WriteLine("(" + p1._x + ", " + p1._y + ")");
+            Console.WriteLine("(" + p2._x + ", " + p2._y + ")");
+            Console.ReadKey();
+        }
+    }
+}
