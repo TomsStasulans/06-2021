c840c96b298962b58725c6feb15a02ca9b73db83
Toms <toms.stasulans@gmail.com>
2021-12-11 15:41:50 +0200

    Exercise 11 done.

--- csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
+++ csharp-basics/exercises/ClassesAndObjects/VideoStore/VideoStore.cs
@@ -5,34 +5,53 @@ namespace VideoStore
 {
     class VideoStore
     {
+        private List<Video> _videoList;
 
         public VideoStore()
         {
-            
+            _videoList = new List<Video>();
         }
 
         public void AddVideo(string title)
         {
-            
+            _videoList.Add(new Video(title));
         }
         
         public void Checkout(string title)
         {
-
+            FindVideo(title).BeingCheckedOut();
         }
 
         public void ReturnVideo(string title)
         {
+            FindVideo(title).BeingReturned();
         }
 
         public void TakeUsersRating(double rating, string title)
         {
-            
+            FindVideo(title).ReceivingRating(rating);
         }
 
         public void ListInventory()
         {
-            
+            foreach (Video film in _videoList)
+            {
+                Console.WriteLine(film.ToString());
+            }
+        }
+
+        public Video FindVideo(string title)
+        {
+            foreach (var item in _videoList)
+            {
+                if (item.Title == title)
+                {
+                    return item;
+                    break;
+                }
+            }
+
+            return null;
         }
     }
 }
