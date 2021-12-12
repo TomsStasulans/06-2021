54c81466b22e2070b8ce2a64c1b1742c85151201
Toms <toms.stasulans@gmail.com>
2021-12-12 18:18:19 +0200

    Exercise 3 done.

---
+++ csharp-basics/exercises/Collections/Exercise 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = new HashSet<string>();
+            while(true)
+            {
+                Console.WriteLine("Please some names: ");
+                string input = Console.ReadLine();
+                if(input == "")
+                {
+                    break;
+                }
+                else
+                {
+                    myList.Add(input);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", myList));
+            Console.ReadKey();
+        }
+    }
+}
