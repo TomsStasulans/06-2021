2c5376a64e70596e4fe232e157e378cce1e21568
Toms <toms.stasulans@gmail.com>
2021-12-12 09:30:13 +0200

    Exercise 12 done.

--- csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
+++ csharp-basics/exercises/ClassesAndObjects/Account/Account.cs
@@ -5,19 +5,20 @@
         private string _name;
         private double _money;
 
-        public Account(string v1, double v2)
+        public Account(string name, double money)
         {
-            
+            _money = money;
+            _name = name;
         }
 
         public double Withdrawal(double i)
         {
-            return i;
+            return _money -= i;
         }
 
         public void Deposit(double i)
         {
-            
+            _money += i;
         }
 
         public double Balance()
