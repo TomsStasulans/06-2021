b08d179f2be4ec5f02c7b52b42d713c1e72bb727
Toms <toms.stasulans@gmail.com>
2021-12-12 16:20:03 +0200

    ListExercise6 done.

--- csharp-basics/exercises/Collections/ListExercise6/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise6/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ListExercise6
 {
@@ -23,8 +20,10 @@ namespace ListExercise6
 
             //TODO: Remove the third element from the list.
 
+            colors.RemoveAt(2);
             Console.WriteLine("After removing third element from the list:");
             Console.WriteLine(string.Join(",", colors));
+            Console.ReadKey();
         }
     }
 }
