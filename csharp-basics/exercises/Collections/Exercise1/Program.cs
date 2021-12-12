8ef3eb45f29d7490c7ee13085a520d332f393428
Toms <toms.stasulans@gmail.com>
2021-12-12 15:06:40 +0200

    Exercise1 done.

--- csharp-basics/exercises/Collections/Exercise1/Program.cs
+++ csharp-basics/exercises/Collections/Exercise1/Program.cs
@@ -1,32 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Exercise1
 {
     class Program
     {
-        /**
-           * Origination:
-           * Audi -> Germany
-           * BMW -> Germany
-           * Honda -> Japan
-           * Mercedes -> Germany
-           * VolksWagen -> Germany
-           * Tesla -> USA
-           */
-
         private static void Main(string[] args)
         {
             string[] array = { "Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Mercedes", "Tesla" };
 
-            //todo - replace array with an List and print out the results
+            List<string> cars = new List<string> { "Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Mercedes", "Tesla" };
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car);
+            }
 
-            //todo - replace array with a HashSet and print out the results
+            Console.WriteLine();
 
-            //todo - replace array with a Dictionary (use brand as key and origination as value) and print out the results
+            HashSet<string> hashCars = new HashSet<string> { "Audi", "BMW", "Honda", "Mercedes", "VolksWagen", "Mercedes", "Tesla" };
+            foreach (string car in hashCars)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.WriteLine();
+
+            Dictionary<string, string> dicCars = new Dictionary<string, string>();
+            dicCars.Add("Audi", "Germany");
+            dicCars.Add("BMW", "Germany");
+            dicCars.Add("Honda", "Japan");
+            dicCars.Add("Mercedes", "Germany");
+            dicCars.Add("VolsWagen", "Germany");
+            dicCars.Add("Tesla", "USA");
+
+            foreach (var car in dicCars)
+            {
+                Console.WriteLine(car.Key + " " + car.Value);
+            }
+
+            Console.ReadKey();
         }
     }
 }
