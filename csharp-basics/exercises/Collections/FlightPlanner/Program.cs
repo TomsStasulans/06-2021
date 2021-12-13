51de02c44101d12ce4bcdc98dae5bfee3cedfdde
Toms <toms.stasulans@gmail.com>
2021-12-13 17:08:38 +0200

    Exercise 6 done.

--- csharp-basics/exercises/Collections/FlightPlanner/Program.cs
+++ csharp-basics/exercises/Collections/FlightPlanner/Program.cs
@@ -1,23 +1,130 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FlightPlanner
 {
     class Program
     {
-        private const string Path = "../FlightPlanner/flights.txt";
+        private const string Path = @"C:\Projects\06-2021\csharp-basics\exercises\Collections\FlightPlanner\flights.txt";
 
         private static void Main(string[] args)
         {
             var readText = File.ReadAllLines(Path);
-            foreach (var s in readText)
+            List<string> flightInfo = new List<string>();
+            FlightsToList(readText, flightInfo);
+            List<string> roundTrip = new List<string>();
+
+            HashSet<string> allCities = new HashSet<string>();
+            foreach (string flight in readText)
             {
-                Console.WriteLine(s);
+                string[] cities = SplitRoute(flight);
+                allCities.Add(cities[0]);
+                allCities.Add(cities[1]);
+
             }
+
+            while (true)
+            {
+                Console.WriteLine("What would you like to do:");
+                Console.WriteLine("To display list of the cities press 1");
+                Console.WriteLine("To exit program press #");
+                var character = Console.ReadKey();
+                char option = character.KeyChar;
+
+                switch (option)
+                {
+                    case '#':
+                        return;
+                    case '1':
+                        DisplayCities(flightInfo);
+                        break;
+                    default:
+                        return;
+                }
+
+                Console.WriteLine("To select a city from which you would like to start press 1");
+                Console.WriteLine("To exit program press #");
+                var secondChar = Console.ReadKey();
+                char secondOption = secondChar.KeyChar;
+
+                switch (secondOption)
+                {
+                    case '#':
+                        return;
+                    case '1':
+                        foreach (var city in allCities)
+                        {
+                            Console.WriteLine(city);
+                        }
+                        break;
+                    default:
+                        return;
+                }
+
+                while (true)
+                {
+                    if(roundTrip.Count == 0)
+                    {
+                        Console.WriteLine("Enter the city you would like to fly from");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter the city you would like to fly to");
+                    }
+
+                    Console.WriteLine("To exit program press #");
+                    string inputCity = Console.ReadLine();
+
+                    if (inputCity == "#")
+                    {
+                        return;
+                    }
+
+                    if (!roundTrip.Contains(inputCity))
+                    {
+                        roundTrip.Add(inputCity);
+                        foreach (string flight in readText)
+                        {
+                            string[] cities = SplitRoute(flight);
+                            if (cities[0] == inputCity)
+                            {
+                                Console.WriteLine(cities[1]);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        roundTrip.Add(inputCity);
+                        Console.WriteLine("Your route will be: ");
+                        DisplayCities(roundTrip);
+                        return;
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void DisplayCities(List<string> cities)
+        {
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city);
+            }
+        }
+
+        private static void FlightsToList(string[] text, List<string> list)
+        {
+            foreach (var s in text)
+            {
+                list.Add(s);
+            }
+        }
+
+        private static string[] SplitRoute(string cities)
+        {
+            return cities.Split(new string[] { " -> " }, StringSplitOptions.None);
         }
     }
 }
