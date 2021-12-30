eefecafd170e9ad2fa6462128bf9471fe0e7d460
Toms <toms.stasulans@gmail.com>
2021-12-30 15:52:53 +0200

    GravityCalculator tested.

---
+++ csharp-basics/exercises/Arithmetic/GravityCalculator.Tests/GravityCalculator.Tests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GravityCalculator.Tests
+{
+    [TestClass]
+    public class GravityCalculatorTests
+    {
+        private CalculateGravity _target;
+
+        [TestMethod]
+        public void FallingTimeIsTenSeconds_CalculateFinalPosition_ShouldBe490comma5()
+        {
+            //Arrange
+            _target = new CalculateGravity();
+            var expected = 490.5;
+            //Act
+            double result = _target.CalculateFinalPositionWhenFallingTenSeconds();
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ csharp-basics/exercises/Arithmetic/GravityCalculator/CalculateGravity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GravityCalculator
+{
+    public class CalculateGravity
+    {
+        private double gravity = -9.81;
+        private double initialVelocity = 0.0;
+        private double fallingTime = 10.0;
+        private double initialPosition = 0.0;
+
+        public CalculateGravity()
+        {
+
+        }
+
+        public double CalculateFinalPositionWhenFallingTenSeconds()
+        {
+            return 0.5 * -(Math.Pow(fallingTime, 2) * gravity) + initialPosition + initialVelocity;
+        }
+    }
+}
