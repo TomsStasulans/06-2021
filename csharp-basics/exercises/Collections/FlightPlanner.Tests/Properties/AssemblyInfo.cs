a7aa8ba1bf39bfb61e4e16d06503f831b85e13d5
Toms <toms.stasulans@gmail.com>
2021-12-30 23:59:42 +0200

    Collections FlightPlanner tested.

---
+++ csharp-basics/exercises/Collections/FlightPlanner.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FlightPlanner.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FlightPlanner.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("22acf4c5-be23-4085-b358-1908caacd558")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ csharp-basics/exercises/Collections/FlightPlanner/FlightPlanner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FlightPlanner
+{
+    public class FlightPlanner
+    {
+        public string DisplayCities(List<string> cities)
+        {
+            return string.Join(" ", cities);
+        }
+
+        public List<string> FlightsToList(string[] text)
+        {
+            return text.ToList();
+        }
+
+        public string[] SplitRoute(string cities)
+        {
+            return cities.Split(new string[] { " -> " }, StringSplitOptions.None);
+        }
+
+        public HashSet<string> AddCitiesToHashset(string[] readText)
+        {
+            var result = new HashSet<string>();
+            foreach (string flight in readText)
+            {
+                string[] cities = SplitRoute(flight);
+                result.Add(cities[0]);
+                result.Add(cities[1]);
+            }
+
+            return result;
+        }
+    }
+}
