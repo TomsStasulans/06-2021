974ca0f4a3f902209829fee894406958bf012afb
Toms <toms.stasulans@gmail.com>
2021-12-30 13:00:27 +0200

    Artithmetics Exercise 1 done.

---
+++ csharp-basics/exercises/Arithmetic/Exercise 1/FifteenMagic.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise_1
+{
+    public class FifteenMagic
+    {
+        private readonly int _firstNumber;
+        private readonly int _secondNumber;
+
+        public FifteenMagic(int firstNumber, int secondNumber)
+        {
+            _firstNumber = firstNumber;
+            _secondNumber = secondNumber;
+        }
+        public bool IsAnyNumberFifteen()
+        {
+            return _firstNumber == 15 || _secondNumber == 15;
+        }
+
+        public bool IsSumOrDifferenceFifteen()
+        {
+            return _firstNumber - _secondNumber == 15 || _firstNumber + _secondNumber == 15;
+        }
+    }
+}
---
+++ csharp-basics/exercises/Arithmetic/Exercise1.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Exercise1.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercise1.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("68bdbdf9-fa89-46cf-a68b-104bfd1aae4d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
